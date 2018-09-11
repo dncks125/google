cff8beda31f115d3086d6815585e72c0a1a23fa5
battleslime <battleslime@gmail.com>
2018-09-11 13:29:13 +0900

    Initial commit

---
+++ src/components/google.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <span id="name"></span> <input type="button" id="loginBtn" value="Checking..." onclick="
+   if(this.value==='Login') {
+    gauth.signIn().then(function(){console.log('gauth.signIn');checkLoginStatus();});
+
+  } else {
+    gauth.signOut().then(function(){console.log('gauth.signOut');checkLoginStatus();});
+
+    }"
+  >
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "google",
+
+
+      methods: {
+
+      },
+      created(){
+
+      }
+
+
+    }
+</script>
+
+<style scoped>
+
+</style>
